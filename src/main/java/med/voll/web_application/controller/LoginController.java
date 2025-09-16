f13c5f9eb791647bb7a3460c840895fbfd1d71e1
Gabriel Soares de Souza <gsoaresdesouza@live.com>
2025-09-15 21:25:38 -0300

    Utilizando os filtros do Spring Security

--- src/main/java/med/voll/web_application/controller/LoginController.java
+++ src/main/java/med/voll/web_application/controller/LoginController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-    @GetMapping
+
+    @GetMapping("/login")
     public String carregaPaginaListagem() {
+
         return "autenticacao/login";
     }
 }
