ad7e42f00a68521aa64e848dfb654ec4f1fb6c57
Gabriel Soares de Souza <gsoaresdesouza@live.com>
2025-10-06 19:02:39 -0300

    Utilizando um codificador de senha

--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,5 +28,10 @@ public class ConfiguracoesSeguranca {
                         .alwaysRemember(true))
                 .csrf(Customizer.withDefaults())
                         .build();
+    }
+
+    @Bean
+    public PasswordEncoder codificadorSenha() {
+        return new BCryptPasswordEncoder();
     }
 }
